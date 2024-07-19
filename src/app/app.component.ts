59dfa1056aa1a4d6374d66d271b517c8ae31fdda
Mike <mgbutts@gmail.com>
2024-07-19 19:16:26 -0400

    merging master to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'd280';
+export class AppComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const svgObject = this.renderer.selectRootElement('#svgMap');
+    svgObject.addEventListener('load', () => {
+      const svgDoc = svgObject.contentDocument;
+      const countries = svgDoc.querySelectorAll('.country');
+
+      countries.forEach((country: Element) => {
+        this.renderer.listen(country, 'mouseover', () => {
+          (country as SVGElement).style.fill = 'yellow';
+        });
+
+        this.renderer.listen(country, 'mouseout', () => {
+          (country as SVGElement).style.fill = '';
+        });
+
+        this.renderer.listen(country, 'click', () => {
+          const countryId = country.getAttribute('id');
+          alert(`You clicked on ${countryId}`);
+          // Add navigation logic here
+        });
+      });
+    });
+  }
 }
+
